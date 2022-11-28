a15c32ee7d787e85746bbfe91e8b40c65ae0b0f9
Yuriy Volkov <sir.volkov@outlook.com>
2022-11-28 17:28:06 +0200

    Homework_2 | Commit_1

---
+++ Homework_2/task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Используя Visual Studio, создайте проект по шаблону Console Application.
+//Напишите программу, в которой создайте переменную  'a'. Значение переменной 'а' должен задавать пользователь.
+//Если а < 10 то выведите 'Верно', а иначе (a > 10) выведите 'Неверно'. 
+
+namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program returns the value");
+            Console.WriteLine(new string('-', 30));
+
+            Console.Write("Input the value: ");
+            string a = Console.ReadLine();
+            double value = double.Parse(a);
+
+            Console.WriteLine(new string('·', 30));
+
+
+            if (value < 10)
+            {
+                Console.WriteLine("Input is true!");
+            }
+            else
+            {
+                Console.WriteLine("Input is false!");
+            }
+
+            Console.WriteLine(new string('-', 30));
+        }
+    }
+}
